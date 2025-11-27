fc82299234e2152bbc2a159959186a377f4cbcce
hmksruti06-code <hmksruti06@gmail.com>
2025-11-27 10:25:54 +0400

    MoodMonitor-Mood

--- CycleCareProject/src/com/cyclecare/wellness/MoodMonitor.java
+++ CycleCareProject/src/com/cyclecare/wellness/MoodMonitor.java
@@ -1,4 +1,32 @@
 package com.cyclecare.wellness;
 
-public class MoodMonitor {
+import com.cyclecare.exceptions.InvalidInputException;
+
+public class MoodMonitor extends Monitor<Mood> {
+
+    public void addMood(String moodName) {
+        try {
+            if (moodName == null || moodName.trim().isEmpty()) {
+                throw new InvalidInputException(" Mood cannot be empty.");
+            }
+            //add record
+            addRecord(new Mood(moodName));
+        }
+        catch (InvalidInputException e) {
+            System.out.println("Error! " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void displayRecords() {
+        if (list.isEmpty()) {
+            System.out.println("No mood records available.");
+            return;
+        }
+
+        System.out.println("Your Mood Records:");
+        for (Mood m : list) {
+            System.out.println("- " + m);
+        }
+    }
 }
