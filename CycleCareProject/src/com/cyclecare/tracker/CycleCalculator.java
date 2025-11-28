2f2c917284618f1819e20deb00c482036eda4731
Asini Vangapally <asini.vangapally@gmail.com>
2025-11-28 11:52:51 +0400

    Code for Average Cycle Length in CycleCalculator

--- CycleCareProject/src/com/cyclecare/tracker/CycleCalculator.java
+++ CycleCareProject/src/com/cyclecare/tracker/CycleCalculator.java
@@ -1,9 +1,39 @@
 package com.cyclecare.tracker;
 
-public class CycleCalculator {
-    public static void main(String[] args) {
-        System.out.println("Cycle calculator");
-        System.out.println("Cycle calculator");
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 
+public class CycleCalculator {
+    private List<LocalDate> periodHistory;
+    private int averageCycleLength;
+
+    public CycleCalculator(List<LocalDate> periodHistory){
+        this.periodHistory = periodHistory;
+        this.averageCycleLength = calculateAverage();
+    }
+    public int getAverageCycleLength() {
+        return averageCycleLength;
+    }
+    public List<LocalDate> getPeriodHistory() {
+        return periodHistory;
+    }
+    public void setPeriodHistory(List<LocalDate> periodHistory) {
+        this.periodHistory = periodHistory;
+        this.averageCycleLength = calculateAverage();
+    }
+    //calculating Average cycle length
+    private int calculateAverage() {
+        if (periodHistory == null || periodHistory.size() < 2) {
+            return 28; // default
+        }
+        long totalDays = 0;
+
+        for (int i = 0; i < periodHistory.size() - 1; i++) {
+            long diff = ChronoUnit.DAYS.between(periodHistory.get(i), periodHistory.get(i + 1));
+            totalDays += diff;
+        }
+
+        return (int) (totalDays / (periodHistory.size() - 1));
     }
 }
