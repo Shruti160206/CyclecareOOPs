23561d622678102030e90c462fe12a2b4334d718
Latha Krishna <lathakrishna100@gmail.com>
2025-11-28 11:19:11 +0400

    Added code in CycleDataProcessor

---
+++ CycleCareProject/src/com/cyclecare/exceptions/InvalidCycleDataException.java
@@ -0,0 +1,7 @@
+package com.cyclecare.exceptions;
+
+public class InvalidCycleDataException extends RuntimeException {
+    public InvalidCycleDataException(String msg) {
+        super(msg);
+    }
+}
