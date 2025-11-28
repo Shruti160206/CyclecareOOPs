8a1a562bc919ae4ee4db21c2ae3d22ec4f580486
Diya Kishore <divdiya06@gmail.com>
2025-11-28 11:04:00 +0400

    Print statement for My cylce Phase

--- CycleCareProject/src/com/cyclecare/tracker/MyCyclePhase.java
+++ CycleCareProject/src/com/cyclecare/tracker/MyCyclePhase.java
@@ -1,7 +1,7 @@
 package com.cyclecare.tracker;
 
 public class MyCyclePhase {
-    public static void main(String[] args){
-        System.out.println("My cycle Phase");
-    }
+   public static void main(String[] args){
+       System.out.println("Diya Kishore");
+   }
 }
