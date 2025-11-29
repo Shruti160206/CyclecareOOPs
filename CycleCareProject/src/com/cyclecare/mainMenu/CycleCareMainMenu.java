29d98ff4e1726719a26e7b39b61ef1d9ab4a45e0
Latha Krishna <lathakrishna100@gmail.com>
2025-11-30 00:38:27 +0400

    Adding codes in the main menu

--- CycleCareProject/src/com/cyclecare/mainMenu/CycleCareMainMenu.java
+++ CycleCareProject/src/com/cyclecare/mainMenu/CycleCareMainMenu.java
@@ -1,5 +1,22 @@
 package com.cyclecare.mainMenu;
 
-public class CycleCareMainMenu {
+import com.cyclecare.tracker.CycleDataProcessor;
+import java.time.LocalDate;
+import java.util.List;
 
+public class CycleCareMainMenu {
+    public static void main(String[] args) throws Exception {
+
+        List<LocalDate> history = List.of(
+                LocalDate.of(2024, 1, 1),
+                LocalDate.of(2024, 1, 30),
+                LocalDate.of(2024, 2, 28)
+        );
+
+        CycleDataProcessor processor = new CycleDataProcessor(history);
+
+        processor.getAverageDifference();
+        processor.getVariance();
+    }
 }
+
